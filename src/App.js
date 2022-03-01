6bd6355d876d56b6486c3bdc7441db49f8e32456
sunakshitejwani <sunakshitejwani@gmail.com>
2022-03-01 15:54:48 +0530

    add chatfeed component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import {ChatEngine} from "react-chat-engine"; //import using destructuring 
-import {projectConfig} from "./env_vars";
-import './App.css';
+import { ChatEngine } from "react-chat-engine"; //import using destructuring
+import ChatFeed from "./components/ChatFeed";
+import { projectConfig } from "./env_vars";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ChatEngine 
+      <ChatEngine
         height="100vh"
         projectID={projectConfig.id}
         userName={projectConfig.userName}
         userSecret={projectConfig.userSecret}
+        renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() =>
+          new Audio(
+            "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+          ).play()
+        }
       />
-     </div>
+    </div>
   );
 }
 
